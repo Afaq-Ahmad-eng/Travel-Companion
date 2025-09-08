afc0ef6550cc863320b110242591ee9f6e2ee60b
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-09-09 01:44:09 +0500

    we setup the server.js file to check this work or not

---
+++ server/server.js
@@ -0,0 +1,12 @@
+//Expternal modules
+import express from "express";
+
+const app = express();
+
+//Port
+const PORT = 3000
+
+app.listen(PORT, () => {
+    
+  console.log(`Server is running on port ${PORT}`);
+});
