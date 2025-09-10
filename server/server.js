20624ff2d2bb12f89298742df7f808d26799d038
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-09-10 23:44:34 +0500

    We set up the server.js file to test whether our created API functions are working as expected or not.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 //Expternal modules
 import express from "express";
+import cors from "cors";
 
+//Internal modules
+import authRoutes from "./src/routes/authRoutes.js";
+
+//Express app initialization
 const app = express();
 
 //Port
-const PORT = 3000
+const PORT = 3001
+
+app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
+app.use("/user/auth", authRoutes);
 
 app.listen(PORT, () => {
     
