a013a2a683f98ecdbd9934ee42b1a0f29b6a54dd
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-09-17 15:09:23 +0500

    We created an app.js file inside the src folder. In this file, we set up our backend server by configuring Express and handling middlewares such as express.json() for parsing JSON requests and cors for cross-origin requests. Finally, we export the configured app to be used in the server.js file.

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+//Expternal modules
+import express from "express";
+import cors from "cors";
+
+//Internal modules
+import userRoutes from "./modules/user/user.routes.js";
+import authRoutes from "./modules/auth/auth.routes.js";
+
+//Express app initialization
+const app = express();
+
+//Middlewares
+app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
+app.use("/user", userRoutes);
+
+app.use('/auth',authRoutes)
+
+export default app;
