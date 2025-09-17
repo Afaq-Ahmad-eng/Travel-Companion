56ba8f05a0dddf99de34ca0e28768c085a84fac5
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-09-17 23:54:43 +0500

    We added http://localhost to the console.log statement to visualize how our endpoint would look.

--- server/server.js
+++ server/server.js
@@ -6,5 +6,5 @@ dotenv.config();
 import app from "./src/app.js";
 app.listen(process.env.SERVER_PORT, () => {
 
-  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+  console.log(`Server is running on port http://localhost:${process.env.SERVER_PORT}`);
 });
