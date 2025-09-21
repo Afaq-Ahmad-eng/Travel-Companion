806b0f3d27f36fb4b71aba05ebde96f8949cac66
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-09-22 01:27:48 +0500

    We adjust this file little bit

--- server/src/utils/secure.js
+++ server/src/utils/secure.js
@@ -2,17 +2,18 @@
 
 //External library for AES encryption
 import CryptoJS from "crypto-js";
+import dotenv from 'dotenv';
 
-
+// Load environment variables from .env file
+dotenv.config();
 
 //Function to encrypt password using AES encryption
 export const encryptData = (plainData) => {
-  const secretKey = SECRET_KEY;
-  return CryptoJS.AES.encrypt(plainData, secretKey).toString();
+  return CryptoJS.AES.encrypt(plainData, process.env.ENCRYPTION_AND_DECRYPTION_SECRET_KEY).toString();
 };
 
 //Function to decrypt data
 export const decryptData = (encryptedData) => {
-  const bytes = CryptoJS.AES.decrypt(encryptedData, SECRET_KEY);
+  const bytes = CryptoJS.AES.decrypt(encryptedData, process.env.ENCRYPTION_AND_DECRYPTION_SECRET_KEY);
   return bytes.toString(CryptoJS.enc.Utf8);
 }
