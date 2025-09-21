094811f1df7436a0dd600ab9418489f1a3951f51
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-09-22 01:09:22 +0500

    We remove the key

--- src/utils/secure.js
+++ src/utils/secure.js
@@ -1,8 +1,7 @@
 //External library for AES encryption
 import CryptoJS from "crypto-js";
 
-//Secret key for AES encryption
-const SECRET_KEY= "642c4e496cfa2dd0112023f0cb9902a3cfc91e544b84718d6010bf65ef37d701"
+
 
 //Function to encrypt password using AES encryption
 export const encryptData = (plainData) => {
