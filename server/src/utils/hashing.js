b30204047dab6b9831aa976c4616f8f283cccf72
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-10-05 09:04:30 +0500

    Updated the hashing.js file, which contains two functions: hashPassword for generating a secure hash of the password, and verifyPassword for verifying whether the provided password is correct. Added try-catch blocks to both functions for improved error handling.

--- server/src/utils/hashing.js
+++ server/src/utils/hashing.js
@@ -1,11 +1,20 @@
 import argon from "argon2";
 
 // Hash password
-export const hashPassword = async (password) => {
-  return await argon.hash(password);
+export const hashPassword = async (password) => {  
+  try {
+    const hash = await argon.hash(password);
+    return hash;
+  } catch (error) {
+    throw new Error("Password hashing failed");
+  }
 };
 
 // Verify password
 export const verifyPassword = async (password, hashedPassword) => {
-  return await argon.verify(hashedPassword, password);
-};
+  try{
+    return await argon.verify(hashedPassword, password);
+  }catch(errorInVerifyPassword){
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
