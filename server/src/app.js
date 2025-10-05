90ba93f7773f6e0500181df1ace7915329b54c65
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-10-05 10:50:57 +0500

    We updated the app.js file to include the cookie-parser middleware and integrated the protectedRoutes middleware to secure endpoints such as the Share Experience form and other protected routes. However, some of the related functionalities are not yet fully implemented,  once they are built, they will be properly integrated and utilized within the app.js file.

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,32 @@
-//Expternal modules
+// src/app.js
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
-//Internal modules
-import userRoutes from "./modules/user/user.routes.js";
-import authRoutes from "./modules/auth/auth.routes.js";
 
-//Express app initialization
+import authRoutes from "./modules/auth/auth.routes.js"; // keep auth public
+import shareRoutes from "./modules/shareExperience/shareExperience.routes.js";
+import { protectedRoutes } from "./middleware/protect.js";
+
 const app = express();
 
-//Middlewares
-app.use(express.json());
-
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE"]
+app.use(cors({ 
+  origin: "http://localhost:3000", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true 
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use("/user", userRoutes);
+// Public auth router (register/login etc)
+app.use("/auth", authRoutes);
 
-app.use('/auth',authRoutes)
+// Protect entire share module, then mount routes inside
+app.use("/share", protectedRoutes, shareRoutes);
+
+// Health
+app.get("/", (req, res) => res.send("API running"));
 
 
 export default app;
