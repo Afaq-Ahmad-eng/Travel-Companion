d6267f125fbcc6d816d543e5adcee33a86d6a296
Afaq-Ahmad-eng <afaqahmad2593@gmail.com>
2025-10-17 00:08:06 +0500

    Added leafletConfig.js file to manage Leaflet configuration for smooth integration with the React application.

---
+++ src/leafletConfig.js
@@ -0,0 +1,15 @@
+import L from "leaflet";
+import markerRetina from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Remove any previous _getIconUrl so bundlers don't break paths
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerRetina,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+export default L;
